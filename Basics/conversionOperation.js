28710a8b7088a80e092a025007ee8789291e78e4
Monu Gupta <123137911+Monugupta1211@users.noreply.github.com>
2024-10-12 13:20:03 +0000

    Conversion of datatypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,58 @@
+// let score ="33";
+// console.log(typeof score);//string
+// console.log(typeof(score));//string
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);//number
+
+
+// let score =33;
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+// let score ="33hello";
+// console.log(typeof score);//string
+// console.log(typeof(score));//string
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);//number
+
+
+// let score =null;
+// console.log(typeof score);// object
+// console.log(typeof(score));//object
+// let valueInNumber = Number(score);//
+// console.log(typeof valueInNumber);//number
+
+// let score =undefined;
+// console.log(typeof score);//undefiled 
+// console.log(typeof(score));//undefined 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);//number
+
+
+// let score =true;
+// console.log(typeof score);//boolean
+// console.log(typeof(score));//boolean
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);//number
+
+/* 
+"33" => 33
+"33abc" => NaN
+true => 1;false => 0
+*/
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)// true
+// 1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+
+let score =33;
+console.log(typeof score);// number
+let valueIsString = String(score)
+console.log(valueIsString)// 33
+console.log(typeof valueIsString);//string
